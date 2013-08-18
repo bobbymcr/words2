ce9a9483db1e4c4d4b31362ccb38c55bef54eb42
Brian Rogers <bobbymcr@bobbymcr.com>
2013-08-18 11:22:41 -0700

    Add WordLoader

---
+++ source/Words2.Core/WordLoader.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WordLoader.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words2
+{
+    using System;
+
+    public static class WordLoader
+    {
+        private static readonly char[] WhitespaceChars = new char[] { ' ', '\t', '\r', '\n' };
+
+        public static void Load(string line, Action<Word> onWordFound)
+        {
+            foreach (string s in line.Split(WhitespaceChars, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (s.Length <= 15)
+                {
+                    Word word = new Word(s);
+                    onWordFound(word);
+                }
+            }
+        }
+    }
+}
---
+++ test/Words2.Test.Unit/WordLoaderTest.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WordLoaderTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words2.Test.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class WordLoaderTest
+    {
+        public WordLoaderTest()
+        {
+        }
+
+        [Fact]
+        public void Load_EmptyString_DoesNothing()
+        {
+            int count = 0;
+            WordLoader.Load(string.Empty, w => ++count);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void Load_TooLong_DoesNothing()
+        {
+            int count = 0;
+            WordLoader.Load("abcdefghijklmnoqrstuvwxyz", w => ++count);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void Load_MixOfWords_FindsOnlyValidWordItemsSeparateByWhitespace()
+        {
+            List<string> words = new List<string>();
+            WordLoader.Load("  a bc    def ghij abcdefghijklmnopqr  klmno\tpqrstu\rvwxy\nz  ", w => words.Add(w.ToString()));
+
+            Assert.Equal(new string[] { "A", "BC", "DEF", "GHIJ", "KLMNO", "PQRSTU", "VWXY", "Z" }, words.ToArray());
+        }
+    }
+}
