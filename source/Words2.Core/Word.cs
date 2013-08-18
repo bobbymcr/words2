ba05989bb0de307e66b1048ce7fcd5c1ff0a8e3e
Brian Rogers <bobbymcr@bobbymcr.com>
2013-08-18 10:32:30 -0700

    Change Word to use bytes on stack instead of String

--- source/Words2.Core/Word.cs
+++ source/Words2.Core/Word.cs
@@ -13,7 +13,22 @@ namespace Words2
 
     public struct Word : IEquatable<Word>, IComparable<Word>, IEnumerable<char>
     {
-        private readonly string word;
+        private readonly byte length;
+        private readonly byte c0;
+        private readonly byte c1;
+        private readonly byte c2;
+        private readonly byte c3;
+        private readonly byte c4;
+        private readonly byte c5;
+        private readonly byte c6;
+        private readonly byte c7;
+        private readonly byte c8;
+        private readonly byte c9;
+        private readonly byte cA;
+        private readonly byte cB;
+        private readonly byte cC;
+        private readonly byte cD;
+        private readonly byte cE;
 
         public Word(string word)
         {
@@ -32,30 +47,91 @@ namespace Words2
                 throw new ArgumentOutOfRangeException("word", "Word cannot be longer than 15 characters.");
             }
 
-            StringBuilder sb = new StringBuilder();
-            foreach (char c in word)
+            this.length = (byte)word.Length;
+            this.c0 = this.c1 = this.c2 = this.c3 = this.c4 = this.c5 = this.c6 = this.c7 = this.c8 = this.c9 = this.cA = this.cB = this.cC = this.cD = this.cE = 0;
+            for (int i = 0; i < this.length; ++i)
             {
+                char c = word[i];
                 char cu = char.ToUpperInvariant(c);
-                int a = cu - 'A';
-                if ((a < 0) || (a > 25))
+                int a = cu - 'A' + 1;
+                if ((a < 1) || (a > 26))
                 {
                     throw new ArgumentException("Word contained an invalid character ('" + c + "').", "word");
                 }
 
-                sb.Append(cu);
+                switch (i)
+                {
+                    case 0x0:
+                        this.c0 = (byte)a;
+                        break;
+                    case 0x1:
+                        this.c1 = (byte)a;
+                        break;
+                    case 0x2:
+                        this.c2 = (byte)a;
+                        break;
+                    case 0x3:
+                        this.c3 = (byte)a;
+                        break;
+                    case 0x4:
+                        this.c4 = (byte)a;
+                        break;
+                    case 0x5:
+                        this.c5 = (byte)a;
+                        break;
+                    case 0x6:
+                        this.c6 = (byte)a;
+                        break;
+                    case 0x7:
+                        this.c7 = (byte)a;
+                        break;
+                    case 0x8:
+                        this.c8 = (byte)a;
+                        break;
+                    case 0x9:
+                        this.c9 = (byte)a;
+                        break;
+                    case 0xA:
+                        this.cA = (byte)a;
+                        break;
+                    case 0xB:
+                        this.cB = (byte)a;
+                        break;
+                    case 0xC:
+                        this.cC = (byte)a;
+                        break;
+                    case 0xD:
+                        this.cD = (byte)a;
+                        break;
+                    case 0xE:
+                        this.cE = (byte)a;
+                        break;
+                }
             }
-
-            this.word = sb.ToString();
         }
 
         public char this[int index]
         {
-            get { return this.word[index]; }
+            get
+            {
+                if (index >= this.length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return (char)(this.GetByteValue(index) + 'A' - 1);
+            }
         }
 
         public override string ToString()
         {
-            return this.word;
+            StringBuilder sb = new StringBuilder(this.length);
+            for (int i = 0; i < this.length; ++i)
+            {
+                sb.Append(this[i]);
+            }
+
+            return sb.ToString();
         }
 
         public override bool Equals(object obj)
@@ -71,27 +147,104 @@ namespace Words2
 
         public override int GetHashCode()
         {
-            return this.word.GetHashCode();
+            ulong x =
+                ((ulong)this.length) |
+                ((ulong)this.c0 << 8) |
+                ((ulong)this.c1 << 16) |
+                ((ulong)this.c2 << 24) |
+                ((ulong)this.c3 << 32) |
+                ((ulong)this.c4 << 40) |
+                ((ulong)this.c5 << 48) |
+                ((ulong)this.c6 << 56);
+            ulong y =
+                ((ulong)this.c7) |
+                ((ulong)this.c8 << 8) |
+                ((ulong)this.c9 << 16) |
+                ((ulong)this.cA << 24) |
+                ((ulong)this.cB << 32) |
+                ((ulong)this.cC << 40) |
+                ((ulong)this.cD << 48) |
+                ((ulong)this.cE << 56);
+
+            return x.GetHashCode() ^ y.GetHashCode();
         }
 
         public bool Equals(Word other)
         {
-            return this.word == other.word;
+            return this.CompareTo(other) == 0;
         }
 
         public int CompareTo(Word other)
         {
-            return string.Compare(this.word, other.word, StringComparison.Ordinal);
+            int n1 = this.length;
+            int n2 = other.length;
+            int m;
+            if (n1 < n2)
+            {
+                m = n1;
+            }
+            else
+            {
+                m = n2;
+            }
+
+            for (int i = 0; i < m; ++i)
+            {
+                int c = this[i].CompareTo(other[i]);
+                if (c != 0)
+                {
+                    return c;
+                }
+            }
+
+            if (n1 < n2)
+            {
+                return -1;
+            }
+            else if (n1 > n2)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         public IEnumerator<char> GetEnumerator()
         {
-            return this.word.GetEnumerator();
+            for (int i = 0; i < this.length; ++i)
+            {
+                yield return this[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
+        }
+
+        private byte GetByteValue(int index)
+        {
+            switch (index)
+            {
+                case 0x0: return this.c0;
+                case 0x1: return this.c1;
+                case 0x2: return this.c2;
+                case 0x3: return this.c3;
+                case 0x4: return this.c4;
+                case 0x5: return this.c5;
+                case 0x6: return this.c6;
+                case 0x7: return this.c7;
+                case 0x8: return this.c8;
+                case 0x9: return this.c9;
+                case 0xA: return this.cA;
+                case 0xB: return this.cB;
+                case 0xC: return this.cC;
+                case 0xD: return this.cD;
+                case 0xE: return this.cE;
+                default: throw new IndexOutOfRangeException();
+            }
         }
     }
 }
