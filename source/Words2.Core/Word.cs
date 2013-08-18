a61804f1d190a3116eccc92fde4bf7e624e9ec08
Brian Rogers <bobbymcr@bobbymcr.com>
2013-08-17 21:41:18 -0700

    Add Word

---
+++ source/Words2.Core/Word.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Word.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words2
+{
+    using System;
+
+    public struct Word : IEquatable<Word>
+    {
+        private readonly string word;
+
+        public Word(string word)
+        {
+            this.word = word.ToUpperInvariant();
+        }
+
+        public void ForEach(Action<char> action)
+        {
+            foreach (char c in this.word)
+            {
+                action(c);
+            }
+        }
+
+        public void ForEach(Func<char, bool> func)
+        {
+            foreach (char c in this.word)
+            {
+                if (!func(c))
+                {
+                    break;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.word;
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool isEqual = false;
+            if (obj is Word)
+            {
+                isEqual = this.Equals((Word)obj);
+            }
+
+            return isEqual;
+        }
+
+        public bool Equals(Word other)
+        {
+            return this.word == other.word;
+        }
+    }
+}
---
+++ test/Words2.Test.Unit/WordTest.cs
@@ -0,0 +1,93 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WordTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words2.Test.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class WordTest
+    {
+        public WordTest()
+        {
+        }
+
+        [Fact]
+        public void FromString_NormalizesToUppercase()
+        {
+            Word a1 = new Word("abc");
+            Word a2 = new Word("abC");
+            Word a3 = new Word("ABC");
+
+            Assert.Equal("ABC", a1.ToString());
+            Assert.Equal("ABC", a2.ToString());
+            Assert.Equal("ABC", a3.ToString());
+        }
+
+        [Fact]
+        public void ForEach_OneChar_RunsForEachChar()
+        {
+            ForEachInnerTest("a", new char[] { 'A' });
+        }
+
+        [Fact]
+        public void ForEach_TwoChars_RunsForEachChar()
+        {
+            ForEachInnerTest("aB", new char[] { 'A', 'B' });
+        }
+
+        [Fact]
+        public void ForEach_ThreeChars_RunsForEachChar()
+        {
+            ForEachInnerTest("aBc", new char[] { 'A', 'B', 'C' });
+        }
+
+        [Fact]
+        public void ForEach_ReturnFalse_BreaksOutOfLoop()
+        {
+            Word w = new Word("aabaa");
+            List<char> items = new List<char>();
+            int count = 0;
+            Func<char, bool> func = delegate(char c)
+            {
+                ++count;
+                return c == 'A';
+            };
+
+            w.ForEach(func);
+
+            Assert.Equal(3, count);
+        }
+
+        [Fact]
+        public void Equals_ComparesCorrectly()
+        {
+            Word a1 = new Word("abc");
+            Word a2 = new Word("abC");
+            Word b1 = new Word("bCd");
+            Word b2 = new Word("Bcd");
+
+            Assert.True(a1.Equals(a2));
+            Assert.True(a2.Equals(a1));
+            Assert.False(a1.Equals(b2));
+            Assert.True(b1.Equals(b2));
+            Assert.True(b2.Equals(b1));
+            Assert.False(a1.Equals(null));
+            Assert.False(a1.Equals("ABC"));
+        }
+
+        private static void ForEachInnerTest(string word, char[] expected)
+        {
+            Word w = new Word(word);
+            List<char> items = new List<char>();
+
+            w.ForEach(c => items.Add(c));
+
+            Assert.Equal(expected, items.ToArray());
+        }
+    }
+}
