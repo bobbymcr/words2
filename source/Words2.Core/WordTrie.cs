54a1caa171acf36a48628e198f9c3c6548fb1fd3
Brian Rogers <bobbymcr@bobbymcr.com>
2013-08-18 12:57:43 -0700

    Add WordTrie

---
+++ source/Words2.Core/WordTrie.cs
@@ -0,0 +1,116 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WordTrie.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words2
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class WordTrie
+    {
+        private readonly Node root;
+
+        public WordTrie(IEnumerable<Word> words)
+            : this()
+        {
+            foreach (Word word in words)
+            {
+                this.Add(word);
+            }
+        }
+
+        public WordTrie()
+        {
+            this.root = new Node();
+        }
+
+        public bool Add(Word item)
+        {
+            Node current = this.root;
+            bool anyAdded = false;
+            for (int i = 0; i < item.Length; ++i)
+            {
+                bool added = current.TryAddChild(item[i], out current);
+                if (added)
+                {
+                    anyAdded = true;
+                }
+            }
+
+            return anyAdded;
+        }
+
+        public bool Remove(Word item)
+        {
+            Node current = this.root;
+            for (int i = 0; i < item.Length; ++i)
+            {
+                bool removed = current.TryRemoveChild(item[i], out current);
+                if (!removed)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool Contains(Word item)
+        {
+            Node current = this.root;
+            for (int i = 0; i < item.Length; ++i)
+            {
+                bool found = current.TryGetChild(item[i], out current);
+                if (!found)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private sealed class Node
+        {
+            private readonly Dictionary<char, Node> children;
+
+            public Node()
+            {
+                this.children = new Dictionary<char, Node>();
+            }
+
+            public bool TryAddChild(char key, out Node child)
+            {
+                bool added = false;
+                if (!this.children.TryGetValue(key, out child))
+                {
+                    added = true;
+                    child = new Node();
+                    this.children.Add(key, child);
+                }
+
+                return added;
+            }
+
+            public bool TryRemoveChild(char key, out Node child)
+            {
+                bool removed = false;
+                if (this.children.TryGetValue(key, out child))
+                {
+                    removed = true;
+                    this.children.Remove(key);
+                }
+
+                return removed;
+            }
+
+            public bool TryGetChild(char key, out Node child)
+            {
+                return this.children.TryGetValue(key, out child);
+            }
+        }
+    }
+}
---
+++ test/Words2.Test.Unit/WordTrieTest.cs
@@ -0,0 +1,126 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WordTrieTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words2.Test.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class WordTrieTest
+    {
+        public WordTrieTest()
+        {
+        }
+
+        [Fact]
+        public void Add_FirstTimeReturnsTrueNextTimeReturnsFalse()
+        {
+            WordTrie trie = new WordTrie();
+
+            Assert.True(trie.Add(new Word("z")));
+            Assert.False(trie.Add(new Word("z")));
+            Assert.False(trie.Add(new Word("z")));
+
+            Assert.True(trie.Add(new Word("zz")));
+            Assert.False(trie.Add(new Word("zz")));
+            Assert.False(trie.Add(new Word("zz")));
+
+            Assert.True(trie.Add(new Word("zzx")));
+            Assert.False(trie.Add(new Word("zzx")));
+            Assert.False(trie.Add(new Word("zzx")));
+
+            Assert.True(trie.Add(new Word("azxy")));
+            Assert.False(trie.Add(new Word("azxy")));
+            Assert.False(trie.Add(new Word("azxy")));
+
+            Assert.True(trie.Add(new Word("zxzxzxzxzxzxzxz")));
+            Assert.False(trie.Add(new Word("zxzxzxzxzxzxzxz")));
+            Assert.False(trie.Add(new Word("zxzxzxzxzxzxzxz")));
+        }
+
+        [Fact]
+        public void Construct_WordSequence_AddsToTrie()
+        {
+            Word w1 = new Word("aaaa");
+            Word w2 = new Word("zzzz");
+
+            WordTrie trie = new WordTrie(new Word[] { w1, w2 });
+
+            Assert.True(trie.Contains(w1));
+            Assert.True(trie.Contains(w2));
+        }
+
+        [Fact]
+        public void Contains_ReturnsTrueIfFoundFalseOtherwise()
+        {
+            WordTrie trie = new WordTrie();
+
+            Assert.False(trie.Contains(new Word("a")));
+
+            trie.Add(new Word("a"));
+
+            Assert.True(trie.Contains(new Word("a")));
+            Assert.False(trie.Contains(new Word("ab")));
+
+            trie.Add(new Word("abc"));
+
+            Assert.True(trie.Contains(new Word("abc")));
+
+            trie.Remove(new Word("abc"));
+
+            Assert.False(trie.Contains(new Word("abc")));
+        }
+
+        [Fact]
+        public void Remove_TrieEmpty_AlwaysReturnsFalse()
+        {
+            WordTrie trie = new WordTrie();
+
+            Assert.False(trie.Remove(new Word("z")));
+            Assert.False(trie.Remove(new Word("zy")));
+            Assert.False(trie.Remove(new Word("zyx")));
+            Assert.False(trie.Remove(new Word("zyxw")));
+            Assert.False(trie.Remove(new Word("abcdefghijklmno")));
+        }
+
+        [Fact]
+        public void Remove_FirstTimeReturnsTrueNextTimeReturnsFalse()
+        {
+            WordTrie trie = new WordTrie();
+
+            trie.Add(new Word("z"));
+
+            Assert.True(trie.Remove(new Word("z")));
+            Assert.False(trie.Remove(new Word("z")));
+            Assert.False(trie.Remove(new Word("z")));
+
+            trie.Add(new Word("zz"));
+
+            Assert.True(trie.Remove(new Word("zz")));
+            Assert.False(trie.Remove(new Word("zz")));
+            Assert.False(trie.Remove(new Word("zz")));
+
+            trie.Add(new Word("zzx"));
+
+            Assert.True(trie.Remove(new Word("zzx")));
+            Assert.False(trie.Remove(new Word("zzx")));
+            Assert.False(trie.Remove(new Word("zzx")));
+
+            trie.Add(new Word("zwxy"));
+
+            Assert.True(trie.Remove(new Word("zwxy")));
+            Assert.False(trie.Remove(new Word("zwxy")));
+            Assert.False(trie.Remove(new Word("zwxy")));
+
+            trie.Add(new Word("vwxy"));
+
+            Assert.True(trie.Remove(new Word("vwxy")));
+            Assert.False(trie.Remove(new Word("vwxy")));
+            Assert.False(trie.Remove(new Word("vwxy")));
+        }
+    }
+}
